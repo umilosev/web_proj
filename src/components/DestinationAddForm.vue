7e957aee6c7f2e67c3eccb6c4692b913b345b057
umilosev <umilosev12822rn@raf.rs>
2024-07-01 00:17:41 +0200

    adding dests

--- src/components/DestinationAddForm.vue
+++ src/components/DestinationAddForm.vue
@@ -7,13 +7,13 @@
       <!-- Name -->
       <div class="form-group">
         <label for="name">Name:</label>
-        <input type="text" id="name" v-model="newDestination.name" required>
+        <input type="text" id="name" v-model="newDestination.ime" required>
       </div>
 
       <!-- Description -->
       <div class="form-group">
         <label for="description">Description:</label>
-        <textarea id="description" v-model="newDestination.description" rows="4" required></textarea>
+        <textarea id="description" v-model="newDestination.opis" rows="4" required></textarea>
       </div>
 
       <!-- Submit Button -->
@@ -31,32 +31,47 @@ export default {
   data() {
     return {
       newDestination: {
-        name: '',
-        description: ''
-      }
+        ime: '',
+        opis: ''
+      },
+      destinations: [] // Initialize the destinations array
     };
   },
   methods: {
     handleSubmit() {
       // Validate if the destination name already exists
-      const existingDestination = this.destinations.find(dest => dest.name === this.newDestination.name);
+      const existingDestination = this.destinations.find(dest => dest.ime === this.newDestination.ime);
       if (existingDestination) {
         alert('Destination with this name already exists.');
         return;
       }
 
       // Call your API to add the destination
-      // Example:
-      // this.$http.post('/api/destinations', this.newDestination)
-      //   .then(response => {
-      //     console.log('Destination added successfully', response.data);
-      //     // Optionally, perform additional actions after successful submission
-      //   })
-      //   .catch(error => {
-      //     console.error('Error adding destination:', error);
-      //     // Handle error scenarios
-      //   });
+
+       this.axios.post('/api/destinacije/add', this.newDestination)
+         .then(response => {
+           console.log('Destination added successfully', response.data);
+           // Optionally, perform additional actions after successful submission
+         })
+         .catch(error => {
+           console.error('Error adding destination:', error);
+           // Handle error scenarios
+        });
+    },
+    fetchDestinations() {
+      this.axios.get('/api/destinacije')
+          .then(response => {
+            console.log('Fetched destinations:', response.data);
+            this.destinations = response.data;
+          })
+          .catch(error => {
+            console.error('Error fetching destinations:', error);
+            // Handle error scenarios
+          });
     }
+  },
+  mounted() {
+    this.fetchDestinations();
   }
 };
 </script>
